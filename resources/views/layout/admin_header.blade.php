704e6aad1fe101342346750977ce3135b82bf5b6
Jan Hähl <jan.haehl@Jans-MacBook-Pro-3.local>
2019-09-02 13:12:22 +0200

    tagmanager also in admin head

--- resources/views/layout/admin_header.blade.php
+++ resources/views/layout/admin_header.blade.php
@@ -17,6 +17,27 @@
     {{--<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js"></script>--}}
     <script src="https://cdn.tiny.cloud/1/ahqrzywhp6m8zsa7s17galk60h0wfw8vpmz31op7w1q4btmh/tinymce/5/tinymce.min.js"></script>
     <script>tinymce.init({selector:'textarea'});</script>
+        <!-- Google Tag Manager -->
+        <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-WZ9Z62Q');
+    </script>
+    <!-- End Google Tag Manager -->
 </head>
 
 <body>
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WZ9Z62Q" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
